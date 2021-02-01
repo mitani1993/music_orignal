23e286c0b4a85dd6edc5bba430743dc573d1bc42
mitani1993 <fytim55@gmail.com>
2021-02-01 22:29:57 +0900

    アクティブハッシュ

---
+++ app/models/area.rb
@@ -0,0 +1,14 @@
+class Area < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: '北海道' },
+    { id: 3, name: '東北' },
+    { id: 4, name: '関東' },
+    { id: 5, name: '中部' },
+    { id: 6, name: '近畿' },
+    { id: 7, name: '中国' },
+    { id: 8, name: '四国' },
+    { id: 9, name: '九州' },
+    { id: 10, name: '沖縄県' }
+  ]
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,10 @@ class User < ApplicationRecord
     validates :area_id
     validates :profession_id
   end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :area
+    belongs_to :profession
+    validates :area_id, numericality: { other_than: 1 }
+    validates :profession_id, numericality: { other_than: 1 } 
 end
