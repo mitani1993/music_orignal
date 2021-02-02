5121532c3756175178f118e100849328e5bedc5c
mitani1993 <fytim55@gmail.com>
2021-02-02 12:31:53 +0900

    編集設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,17 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    user_path(resource)
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path 
+  end
+
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :area_id, :profession_id])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :area_id, :profession_id, :youtube, :profile, :image])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,17 @@ class User < ApplicationRecord
     validates :profession_id
   end
 
+  def update_without_current_password(params, *options)
+    if params[:password].blank? && params[:password_confirmation].blank?
+      params.delete(:password)
+      params.delete(:password_confirmation)
+    end
+
+    result = update(params, *options)
+    clean_up_passwords
+    result
+  end
+
   extend ActiveHash::Associations::ActiveRecordExtensions
     belongs_to :area
     belongs_to :profession
