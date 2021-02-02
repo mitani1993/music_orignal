e7f066a5aaae273e7bb0f6a39b0539d6549ecc7d
mitani1993 <fytim55@gmail.com>
2021-02-02 16:04:49 +0900

    うまくいかない場合

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて半角で設定してください'
+  validates :password, presence: true, on: :create
+
   with_options presence: true do
     validates :name
     validates :area_id
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe User, type: :model do
   describe '#create' do
     before do
       @user = FactoryBot.build(:user)
+      sleep 0.5
     end
 
     context 'うまくいく場合' do
@@ -24,6 +25,87 @@ RSpec.describe User, type: :model do
       it 'プロフィール画像が空でも登録できる' do
         @user.image = nil
         expect(@user).to be_valid
+      end
+    end
+
+    context 'うまくいくいかない場合' do
+      it '名前が空では登録できない' do
+        @user.name = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Name can't be blank")
+      end
+
+      it 'メールアドレスが空では登録できない' do
+        @user.email = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
+      end
+
+      it 'メールアドレスが一意性であること' do
+        @user.save
+        another_user = FactoryBot.build(:user, email: @user.email)
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include('Email has already been taken')
+      end
+
+      it 'メールアドレスは、@を含む必要があること' do
+        @user.email = 'example.com'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Email is invalid')
+      end
+
+      it 'パスワード空では登録できない' do
+        @user.password = nil
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+
+      it 'パスワードは、6文字以上での入力が必須であること（6文字が入力されていれば、登録が可能なこと）' do
+        @user.password = 'aaa12'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+      end
+
+      it 'パスワードは、半角英数字混合での入力が必須であること（半角英数字が混合されていれば、登録が可能なこと）' do
+        @user.password = 'aaaaaa'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password には英字と数字の両方を含めて半角で設定してください')
+      end
+
+      it 'パスワードは、確認用を含めて2回入力すること' do
+        @user.password_confirmation = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+
+      it 'パスワードとパスワード（確認用）は、値の一致が必須であること' do
+        @user.password_confirmation = 'abc123'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+
+      it 'パスワードは、全角では登録できないこと' do
+        @user.password = 'ＡＢＣ123'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password には英字と数字の両方を含めて半角で設定してください')
+      end
+
+      it 'パスワードは、数字のみでは登録できないこと' do
+        @user.password = '123456'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password には英字と数字の両方を含めて半角で設定してください')
+      end
+
+      it '活動地域が空では登録できない' do
+        @user.area_id = 1
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Area must be other than 1")
+      end
+
+      it '活動地域が空では登録できない' do
+        @user.profession_id = 1
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Profession must be other than 1")
       end
     end
   end
