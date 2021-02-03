8e98337bb59cca9d31e6715b5e46bb9789dea2b8
mitani1993 <fytim55@gmail.com>
2021-02-03 12:06:44 +0900

    indexページ作成

--- app/controllers/matching_controller.rb
+++ app/controllers/matching_controller.rb
@@ -2,5 +2,10 @@ class MatchingController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    #フォローしているユーザーの取得
+    follow_user_ids = Relationship.where(user_id: current_user.id).pluck(:follow_id)
+    #相互フォローのユーザーの取得
+    @match_users = Relationship.where(user_id: follow_user_ids, follow_id: current_user.id).map(&:user)
+    @user = User.find(current_user.id)
   end
 end
