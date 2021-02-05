487423bf45ee3cfa5ec38b04aad4899917118f79
mitani1993 <fytim55@gmail.com>
2021-02-05 15:34:14 +0900

    アソシエーション

--- Gemfile
+++ Gemfile
@@ -60,3 +60,7 @@ gem 'mini_magick'
 gem 'image_processing', '~> 1.2'
 gem 'factory_bot_rails'
 gem 'faker'
+gem 'omniauth-facebook'
+gem 'omniauth-google-oauth2'
+gem "omniauth-rails_csrf_protection"
+gem 'omniauth', '~>1.9.1'
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
 
   #アソシエーション
+  has_many :sns_credentials
   has_many :room_users
   has_many :chat_rooms, through: :room_users
   has_many :messages
