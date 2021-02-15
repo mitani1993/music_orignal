cfb75407a0b49fc36c944317e965fe0e799d6ca0
mitani1993 <fytim55@gmail.com>
2021-02-15 14:01:20 +0900

    チャットルーム修正

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -5,7 +5,7 @@ class ChatRoomsController < ApplicationController
     #自分がいるチャットルームを取得
     current_user_rooms = RoomUser.where(user_id: current_user.id).map(&:chat_room)
     #自分がいるチャットルームでかつ、マッチング一覧ページからクリックしたユーザーがいるチャットルームを取得
-    chat_room = RoomUser.where(user_id: current_user_rooms, user_id: params[:user_id]).map(&:chat_room).first
+    chat_room = RoomUser.where(chat_room: current_user_rooms, user_id: params[:user_id]).map(&:chat_room).first
     if chat_room.blank?
       chat_room = ChatRoom.create
       RoomUser.create(chat_room: chat_room, user_id: current_user.id)
